dcec450cc0ebc37072b79defe8c4afbd3fff8e0e
Mivva <czdf41@gmail.com>
2026-02-10 16:32:34 +0700

    Add files via upload

---
+++ task_7.py
@@ -0,0 +1,23 @@
+class Animal:  # 1 usage
+
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    def eat(self, food):
+        self.weight += food
+        return self.weight
+
+    def info(self):
+        print(f"{self.name}, {self.age} years, {self.weight} kg")
+
+
+class Cat(Animal):
+    
+    def __init__(self, name, age, weight, breed):
+        super().__init__(name, age, weight)
+        self.breed = breed
+
+    def bark(self):
+        print("MEOW!")
